e9c907572f5aaa0573bcc405720fd498ee45b9f7
Jonathan Peterson <JPeterson462@gmail.com>
2020-02-28 18:19:13 -0500

    JP: Added libraries and got images added to document

---
+++ src/com/digiturtle/compiler/DocxBuilder.java
@@ -0,0 +1,88 @@
+package com.digiturtle.compiler;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.util.Units;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+public class DocxBuilder {
+	
+	private XWPFDocument document;
+	
+	private XWPFParagraph p;
+	
+	private XWPFRun r;
+	
+	private String filename;
+	
+	private FileOutputStream stream;
+	
+	public DocxBuilder(String filename) {
+		document = new XWPFDocument();
+		p = document.createParagraph();
+		r = p.createRun();
+		this.filename = filename;
+	}
+	
+	public void addImageToWordDocument(File imageFile) throws IOException, InvalidFormatException {
+		BufferedImage bimg1 = ImageIO.read(imageFile);
+		int width = bimg1.getWidth();
+		int height = bimg1.getHeight();  
+
+		String imgFile = imageFile.getName(); 
+		int imgFormat = getImageFormat(imgFile);
+		double imgRatio = (double) height / (double) width;
+		int imageDocWidth = 540; // 7.5 inches * 72 points = 540
+		int imageDocHeight = (int) ((double) imageDocWidth * imgRatio);
+		
+		r.addPicture(new FileInputStream(imageFile), imgFormat, imgFile, Units.toEMU(imageDocWidth), Units.toEMU(imageDocHeight));
+	}
+
+	private int getImageFormat(String imgFileName) {
+		int format;
+		if (imgFileName.endsWith(".emf"))
+			format = XWPFDocument.PICTURE_TYPE_EMF;
+		else if (imgFileName.endsWith(".wmf"))
+			format = XWPFDocument.PICTURE_TYPE_WMF;
+		else if (imgFileName.endsWith(".pict"))
+			format = XWPFDocument.PICTURE_TYPE_PICT;
+		else if (imgFileName.endsWith(".jpeg") || imgFileName.endsWith(".jpg"))
+			format = XWPFDocument.PICTURE_TYPE_JPEG;
+		else if (imgFileName.endsWith(".png"))
+			format = XWPFDocument.PICTURE_TYPE_PNG;
+		else if (imgFileName.endsWith(".dib"))
+			format = XWPFDocument.PICTURE_TYPE_DIB;
+		else if (imgFileName.endsWith(".gif"))
+			format = XWPFDocument.PICTURE_TYPE_GIF;
+		else if (imgFileName.endsWith(".tiff"))
+			format = XWPFDocument.PICTURE_TYPE_TIFF;
+		else if (imgFileName.endsWith(".eps"))
+			format = XWPFDocument.PICTURE_TYPE_EPS;
+		else if (imgFileName.endsWith(".bmp"))
+			format = XWPFDocument.PICTURE_TYPE_BMP;
+		else if (imgFileName.endsWith(".wpg"))
+			format = XWPFDocument.PICTURE_TYPE_WPG;
+		else {
+			return 0;
+		}
+		return format;
+	}
+	
+	public void close() throws IOException {
+		stream = new FileOutputStream(filename);
+		document.write(stream);
+		stream.close();
+		document.close();
+	}
+
+}
--- src/com/digiturtle/compiler/DocxImageCompiler.java
+++ src/com/digiturtle/compiler/DocxImageCompiler.java
@@ -1,9 +1,34 @@
 package com.digiturtle.compiler;
 
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
+
 public class DocxImageCompiler {
-	
-	public static void main(String[] args) {
-		
+
+
+	public static void main(String[] args) throws Exception {
+		JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+		jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int returnValue = jfc.showOpenDialog(null);
+		if (returnValue == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = jfc.getSelectedFile();
+			System.out.println(selectedFile.getAbsolutePath());
+			
+			DocxBuilder builder = new DocxBuilder("test1.docx");
+			
+			builder.addImageToWordDocument(new File("test/screen a 1.png"));
+			builder.addImageToWordDocument(new File("test/screen a 2.png"));
+			builder.addImageToWordDocument(new File("test/screen a 3.png"));
+			builder.addImageToWordDocument(new File("test/screen a 4.png"));
+			
+			builder.close();			
+		}
+		else {
+			throw new Exception("Directory required!");
+		}
 	}
+
+	
 
 }
